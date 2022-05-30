1ec352c2ba57e31ae3aa251d4d96769538efe6dc
Thiago2703 <thiagocardosorr@gmail.com>
2022-05-29 22:04:54 -0400

    change

--- dist/main.node-cjs.js
+++ dist/main.node-cjs.js
@@ -1100,9 +1100,9 @@ var File = class extends import_events2.EventEmitter {
     const apiStart = options.returnCiphertext ? start : start - start % 16;
     let end = options.end || null;
     const maxConnections = options.maxConnections || 4;
-    const initialChunkSize = options.initialChunkSize || 128 * 1024 * 5;
-    const chunkSizeIncrement = options.chunkSizeIncrement || 128 * 1024 * 5;
-    const maxChunkSize = options.maxChunkSize || 1024 * 1024 * 5;
+    const initialChunkSize = options.initialChunkSize || 128 * 1024;
+    const chunkSizeIncrement = options.chunkSizeIncrement || 128 * 1024;
+    const maxChunkSize = options.maxChunkSize || 1024 * 1024;
     console.log("maxConnections =>", maxConnections);
     console.log("initialChunkSize =>", initialChunkSize);
     console.log("chunkSizeIncrement =>", chunkSizeIncrement);
@@ -1274,6 +1274,9 @@ var File = class extends import_events2.EventEmitter {
         }
       };
       for (let i2 = 0; i2 < maxConnections; i2++) {
+        getChunk();
+        getChunk();
+        getChunk();
         getChunk();
       }
     });
